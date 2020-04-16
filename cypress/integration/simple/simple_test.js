050b3f5c0b9678db472d857b5739c62a3e8ce02f
Adrien <adrien.zemma@epitech.eu>
2020-04-16 21:40:21 +0200

    adding test by cypress

--- cypress/integration/simple/simple_test.js
+++ cypress/integration/simple/simple_test.js
@@ -1,6 +1,29 @@
-describe('My First test', function () {
-    it('Does not do much', function () {
-        cy.visit('https://ca4106-project-272809.appspot.com/');
-        cy.get(".card-header");
+describe('Tests', function () {
+    const itemByPage = 6;
+    const testMovieId = "tt0120689";
+    const testMovie = "The Green Mile";
+    it('Is site UP', function () {
+        cy.visit('/');
+    });
+    it('Item by page', function () {
+
+        cy.get(".card-header").its('length').should('eq', itemByPage);
+        cy.get(".card-body").its('length').should('eq', itemByPage);
+        cy.get(".card-footer").its('length').should('eq', itemByPage);
+    });
+    it('Add element', function () {
+        cy.get('#movieTitle').type(testMovie + ' {enter}');
+        cy.contains(testMovie).click();
+        cy.contains(testMovie);
+        cy.wait(500);
+        cy.get("#" + testMovieId + " > .modal-dialog > .modal-content > .modal-header > .close > span").click();
+    });
+    it('delete Movie', function () {
+        cy.get('#movieTitle').type(testMovie + ' {enter}');
+        cy.contains(testMovie).click();
+        cy.contains(testMovie);
+        cy.wait(500);
+        cy.get("#" + testMovieId + " > .modal-dialog > .modal-content > .modal-body > form > .float-right").click()
+        cy.contains(testMovie).should('not')
     })
 });
